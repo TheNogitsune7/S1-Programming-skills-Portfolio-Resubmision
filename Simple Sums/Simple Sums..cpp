82286c3d336eaa0e568ab391f0eae5ceda1d824a
TheNogitsune7 <teodorpanev596@gmail.com>
2026-07-07 12:13:11 +0100

    assignment

---
+++ Simple Sums/Simple Sums..cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firstNumber = 8;       // 1) integer with value 8
+    int secondNumber = 10;     // 2) integer with value 10
+    int sum = firstNumber + secondNumber;  // 3) add the two
+
+    cout << sum << endl;       // 4) print the sum to the console
+
+    return 0;
+}
